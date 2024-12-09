f9ab27299aff1822ca05efae9676393869dad594
syed Anees <167554933+SyedAnees55@users.noreply.github.com>
2024-12-10 00:39:09 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Ceramics from "./components/ceramics";
 import Product from "./components/product";
 import Benefit from "./components/benefit";
 import Touch from "./components/touch";
-import ProductListing from './productlisting';
+import ProductListing from './productlisting/page';
 
 
 export default function Home() {
